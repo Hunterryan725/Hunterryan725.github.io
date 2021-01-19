eca0cdcf418dca62b7803bda63be579afdcc9407
Hunterryan725 <hunter.contos@yale.edu>
2021-01-19 00:47:03 -0500

    Commit 23

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
 	function setThemeFromCookie() {
 		// Check if the cookie is set 
-		if (typeof Cookies.get('mode') == "undefined" ) {
+		if (typeof Cookies.get('mode') !== "undefined" ) {
 			$('body').addClass("dark-mode");
 			$('#darkmode').attr('checked', true); // toggle change
 			console.log('Cookie: dark mode' );
@@ -14,6 +14,19 @@ $(document).ready(function() {
 			console.log('Cookie: light mode' );
 		}
 	}
+
+	// function setThemeFromCookie() {
+	// 	// Check if the cookie is set 
+	// 	if (typeof Cookies.get('mode') == "undefined" ) {
+	// 		$('body').addClass("dark-mode");
+	// 		$('#darkmode').attr('checked', true); // toggle change
+	// 		console.log('Cookie: dark mode' );
+	// 	} else {
+	// 		$('body').removeClass("dark-mode");
+	// 		$('#darkmode').attr('checked', false); // toggle change
+	// 		console.log('Cookie: light mode' );
+	// 	}
+	// }
 	
 	setThemeFromCookie();
 	
