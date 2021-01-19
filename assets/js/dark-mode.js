382207885a8f0b9815655da797f59697d79be890
Hunterryan725 <hunter.contos@yale.edu>
2021-01-19 01:42:56 -0500

    Commit 40

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -2,9 +2,22 @@
 
 $(document).ready(function() {
 
+	function setThemeFromCookie() {
+		// Check if the cookie is set 
+		if (typeof Cookies.get('mode') !== "undefined" ) {
+			$('body').removeClass("dark-mode");
+			$('#darkmode').attr('checked', false); // toggle change
+			console.log('Cookie: light mode' );
+		} else {
+
+			$('body').addClass("dark-mode");
+			$('#darkmode').attr('checked', true); // toggle change
+			console.log('Cookie: dark mode' );
+		}
+	}
 	// function setThemeFromCookie() {
 	// 	// Check if the cookie is set 
-	// 	if (typeof Cookies.get('mode') !== "undefined" ) {
+	// 	if (typeof Cookies.get('mode') == "undefined" ) {
 	// 		$('body').addClass("dark-mode");
 	// 		$('#darkmode').attr('checked', true); // toggle change
 	// 		console.log('Cookie: dark mode' );
@@ -14,32 +27,22 @@ $(document).ready(function() {
 	// 		console.log('Cookie: light mode' );
 	// 	}
 	// }
-	function setThemeFromCookie() {
-		// Check if the cookie is set 
-		if (typeof Cookies.get('mode') == "undefined" ) {
-			$('body').addClass("dark-mode");
-			$('#darkmode').attr('checked', true); // toggle change
-			console.log('Cookie: dark mode' );
-		} else {
-			$('body').removeClass("dark-mode");
-			$('#darkmode').attr('checked', false); // toggle change
-			console.log('Cookie: light mode' );
-		}
-	}
 	
 	setThemeFromCookie();
 	
 	$('#darkmode').on('change', function(e){
 
 		if ($(this).is(':checked')) {
-			$('body').addClass('dark-mode');
-			//Set cookies for 7 days 
-			Cookies.set('mode', 'dark-mode', { expires: 7 });
-			
-		} else {
 			$('body').removeClass('dark-mode');
 			//Remove cookies
 			Cookies.remove('mode');
+			
+			
+		} else {
+
+			$('body').addClass('dark-mode');
+			//Set cookies for 7 days 
+			Cookies.set('mode', 'dark-mode', { expires: 7 });
 		}
 
 	});
